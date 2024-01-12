f6470e8687046dfbab7f8200415999b905a079b2
Thusi123 <155604493+Thusi123@users.noreply.github.com>
2024-01-12 14:49:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # main.tf
 
 provider "aws" {
-  region = "us-west-1"  # You can change the region as needed
+  region = "eu-west-1"  # You can change the region as needed
   access_key ="AKIATZ37TT5ADRGQ77ME"
   secret_key ="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"
 
@@ -9,7 +9,7 @@ provider "aws" {
 
 # Create an EC2 instance
 resource "aws_instance" "crusers99_app_sever" {
-  ami           = "ami-0ce2cb35386fc22e9"  # Replace with your desired AMI ID
+  ami           = "ami-0905a3c97561e0b69"  # Replace with your desired AMI ID
   instance_type = "t2.micro"
   
   tags = {
