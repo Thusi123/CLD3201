27072dca4ba6e1d3d2d17a9c50be4da56c15660f
Thusi123 <155604493+Thusi123@users.noreply.github.com>
2024-01-12 14:36:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # main.tf
 
 provider "aws" {
-  region = "us-west-2"  # You can change the region as needed
+  region = "us-west-1"  # You can change the region as needed
   access_key ="AKIATZ37TT5ADRGQ77ME"
   secret_key ="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"
 
@@ -9,7 +9,7 @@ provider "aws" {
 
 # Create an EC2 instance
 resource "aws_instance" "crusers99_app_sever" {
-  ami           = "ami-008fe2fc65df48dac"  # Replace with your desired AMI ID
+  ami           = "ami-0ce2cb35386fc22e9"  # Replace with your desired AMI ID
   instance_type = "t2.micro"
   
   tags = {
@@ -18,7 +18,7 @@ resource "aws_instance" "crusers99_app_sever" {
     }
 }
 # Create an S3 bucket
-resource "aws_s3_bucket" "crusers990025-tfstate-bkt" {
+resource "aws_s3_bucket" "crusers9912-tfstate-bkt" {
   bucket = "crusers990025-tfstate-bkt"  # Change this to a globally unique name
   acl    = "private"
 
